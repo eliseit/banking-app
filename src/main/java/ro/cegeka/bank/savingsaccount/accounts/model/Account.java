e925b324786fcff494e81795c808271cce1cbd41
Elisei Tataru <elisei.tataru@gmail.com>
2020-04-02 09:06:23 +0300

    [Elisei](BNK-007) Add account domain entity

---
+++ src/main/java/ro/cegeka/bank/savingsaccount/accounts/model/Account.java
@@ -0,0 +1,14 @@
+package ro.cegeka.bank.savingsaccount.accounts.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+}
---
+++ src/main/java/ro/cegeka/bank/savingsaccount/accounts/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package ro.cegeka.bank.savingsaccount.accounts.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import ro.cegeka.bank.savingsaccount.accounts.model.Account;
+
+public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+
+
+}
