5af038daedbc48d6e3c3ca9d4ba8cfb4e11e16b0
Elisei Tataru <elisei.tataru@gmail.com>
2020-04-02 15:45:00 +0300

    [Elisei](BNK-008) basic form

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,38 @@
+let setup = {
+    today: Date,
+    collapsed: true
+};
+
+(function today() {
+
+    setup.today = moment(new Date()).format('DD/MM/YYYY');
+    let formattedDate = setup.today.toString();
+
+    let localDateDisplay = document.querySelector("#localDateTime");
+    localDateDisplay ? localDateDisplay.innerHTML = formattedDate : "none";
+    console.log(localDateDisplay);
+
+    let dateObj = document.querySelector('#starting');
+    if (dateObj) {
+        dateObj.setAttribute("min", formattedDate);
+        dateObj.setAttribute("placeholder", formattedDate);
+    }
+
+    return setup;
+})();
+
+
+function activateForm() {
+    let buttonActivateForm = document.querySelector('#activateForm');
+    let section = document.querySelector("#newAccount");
+
+    if (setup.collapsed) {
+        section.removeAttribute("hidden");
+        setup.collapsed = !setup.collapsed;
+        buttonActivateForm.innerHTML = "I changed my mind!";
+    } else {
+        section.setAttribute("hidden", "hidden");
+        setup.collapsed = !setup.collapsed;
+        buttonActivateForm.innerHTML = "Create Savings Account!";
+    }
+}
