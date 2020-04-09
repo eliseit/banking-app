767a41c4bac7d0aa382da43ba560c5a95235ed39
Elisei Tataru <elisei.tataru@gmail.com>
2020-04-09 12:29:03 +0300

    [Elisei](BNK-017) add user entity link to accounts

--- src/main/java/ro/cegeka/bank/savingsaccount/accounts/model/Account.java
+++ src/main/java/ro/cegeka/bank/savingsaccount/accounts/model/Account.java
@@ -1,32 +1,136 @@
 package ro.cegeka.bank.savingsaccount.accounts.model;
 
-import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import ro.cegeka.bank.savingsaccount.users.User;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import javax.persistence.*;
 import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.Currency;
+import java.time.LocalDate;
 
 @Entity
-@Data
 @ToString
+@NoArgsConstructor
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    private Currency currency;
+    private String currency;
 
     private BigDecimal amount;
 
+    @Enumerated(EnumType.STRING)
     private AccountType type;
 
-    private Instant startingWith;
+    private LocalDate startingWith;
 
+    @Enumerated(EnumType.STRING)
     private Frequency frequency;
+
+    @ManyToOne
+    private User user;
+
+    private Account(AccountBuilder builder) {
+        this.id = builder.id;
+        this.user = builder.user;
+        this.currency = builder.currency;
+        this.amount = builder.amount;
+        this.type = builder.type;
+        this.startingWith = builder.startingWith;
+        this.frequency = builder.frequency;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public AccountType getType() {
+        return type;
+    }
+
+    public LocalDate getStartingWith() {
+        return startingWith;
+    }
+
+    public Frequency getFrequency() {
+        return frequency;
+    }
+
+    public static class AccountBuilder {
+
+        private long id;
+
+        private User user;
+
+        private String currency;
+
+        private BigDecimal amount;
+
+        private AccountType type;
+
+        private LocalDate startingWith;
+
+        private Frequency frequency;
+
+        public static AccountBuilder account() {
+            return new AccountBuilder();
+        }
+
+        public AccountBuilder withId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public AccountBuilder withUser(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public AccountBuilder withCurrency(String currency) {
+            CurrencyUnit currencyUnit = Monetary.getCurrency(currency);
+            this.currency = currencyUnit.getCurrencyCode();
+            return this;
+        }
+
+        public AccountBuilder withAmount(BigDecimal amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public AccountBuilder withType(AccountType type) {
+            this.type = type;
+            return this;
+        }
+
+        public AccountBuilder withStartingDate(LocalDate startingWith) {
+            this.startingWith = startingWith;
+            return this;
+        }
+
+        public AccountBuilder withFrequency(Frequency frequency) {
+            this.frequency = frequency;
+            return this;
+        }
+
+        private Account buildInternal() {
+
+            return new Account(this);
+        }
+
+        public final Account build() {
+            return buildInternal();
+        }
+    }
 }
--- src/main/java/ro/cegeka/bank/savingsaccount/users/User.java
+++ src/main/java/ro/cegeka/bank/savingsaccount/users/User.java
@@ -1,14 +1,71 @@
 package ro.cegeka.bank.savingsaccount.users;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ro.cegeka.bank.savingsaccount.accounts.model.Account;
+
+import javax.persistence.*;
+import java.util.Set;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newHashSet;
 
 @Entity
+@ToString
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    private String name;
+
+    @OneToMany(cascade = {CascadeType.PERSIST})
+    private Set<Account> accounts = newHashSet();
+
+    private User(UserBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.accounts = builder.accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public static class UserBuilder {
+
+
+        public long id;
+        public String name;
+        public Set<Account> accounts;
+
+        public static UserBuilder user() {
+            return new UserBuilder();
+        }
+
+        public UserBuilder withId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder withAccounts(Set<Account> accounts) {
+            this.accounts = accounts;
+            return this;
+        }
+
+        private User buildInternal() {
+            return new User(this);
+        }
+
+        public final User build() {
+            return buildInternal();
+        }
+    }
+
 }
